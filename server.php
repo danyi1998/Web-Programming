6c6e92f8c1a9aa82ef71de22646eeb1a78472075
danyi1998 <46101155+danyi1998@users.noreply.github.com>
2019-04-14 19:53:01 +0800

    Add files via upload

--- server.php
+++ server.php
@@ -1,62 +1,64 @@
 <?php
     //read in the variables
-    $name = $_POST["name"]
+    $name = $_POST["name"];
 
-    $numOfApples = $_POST["apples"]
-    $numOfOranges = $_POST["oranges"]
-    $numOfBananas = $_POST["bananas"]
+    $numOfApples = $_POST["apples"];
+    $numOfOranges = $_POST["oranges"];
+    $numOfBananas = $_POST["bananas"];
 
-    $paymentMethod = $_POST["payment"] 
+    $paymentMethod = $_POST["payment"];
     
     //compute the total cost 
-    $cost = $numOfApples*0.69 + $numOfOranges*0.59 + $numOfBananas*0.39
+    $cost = $numOfApples*0.69 + $numOfOranges*0.59 + $numOfBananas*0.39;
     
     //define the file
-    $fname = "order.txt" 
-
-    #update the file to contain the total numbers of apples, oranges, and bananas bought by ALL users 
-    
-    //open the file
-    $fptr = fopen($fname, w+) 
+    $fname = "order.txt"; 
 
     //capture the size of the file
-    $fsize = filesize($fname) 
+    $fsize = filesize($fname);
 
     //if file is empty, write to it directly 
     if ($fsize==0)
     {
-        $results = "Total number of apples: " + $numOfApples 
-        + "\nTotal number of oranges: " + $numOfOranges 
-        + "\nTotal number of bananas: " + $numOfBananas
-        fwrite($fptr, $results)
+        //open the file for writing
+        $fptr = fopen($fname, "w");
+        //write to the file
+        fwrite($fptr, "Total number of apples: $numOfApples\r\n");
+        fwrite($fptr, "Total number of oranges: $numOfOranges\r\n");
+        fwrite($fptr, "Total number of bananas: $numOfBananas\r\n");
     }
 
     //if file is not empty 
     else
-    {
+    { 
+        //open the file for reading 
+        $fptr = fopen($fname, "r");
+
         //read the contents
-        $contents = fread($fptr, $fsize)
+        $contents = fread($fptr, $fsize);
 
         //capture the current number of apples, oranges, bananas
-        $split = explode("\n", $contents)
-        $currentApples = explode(":", $split[0])[1] 
-        $currentOranges = explode(":", $split[1])[1] 
-        $currentBananas = explode(":", $split[2])[1] 
+        $split = explode("\r\n", $contents);
+        $currentApples = explode(":", $split[0])[1];
+        $currentOranges = explode(":", $split[1])[1]; 
+        $currentBananas = explode(":", $split[2])[1];  
 
         //add the additional apples, oranges, and bananas to the current numbers
-        $currentApples = $currentApples + $numOfApples
-        $currentOranges = $currentOranges + $numOfOranges
-        $currentBananas = $currentBananas + $numOfBananas
+        $currentApples = $currentApples + $numOfApples;
+        $currentOranges = $currentOranges + $numOfOranges;
+        $currentBananas = $currentBananas + $numOfBananas;
+
+        //open the file for writing
+        $fptr = fopen($fname, "w");
 
         //write the new numbers of apples, oranges, and bananas to the file
-        $results = "Total number of apples: " + $currentApples 
-        + "\nTotal number of oranges: " + $currentOranges
-        + "\nTotal number of bananas: " + $currentBananas
-        fwrite($fptr, $results) 
+        fwrite($fptr, "Total number of apples: $currentApples\r\n");
+        fwrite($fptr, "Total number of oranges: $currentOranges\r\n");
+        fwrite($fptr, "Total number of bananas: $currentBananas\r\n"); 
     }
 
     //close the file
-    fclose($fptr) 
+    fclose($fptr); 
     
 
     
@@ -90,10 +92,10 @@
                 </tr>
 
                 <tr>
-                    <td>Payment Method</td>
+                    <td>Payment</td>
                     <td>".$paymentMethod."</td>
                 </tr>
             </table>
         </body>
-    </html>"
+    </html>";
 ?>
